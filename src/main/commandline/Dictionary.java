203f7fe369b04c5d3bd99d092d61100384b71427
Đoàn Thanh Sơn <sondt.uet@gmail.com>
2021-10-03 01:25:56 +0700

    Cải tiến lần 1
    Sửa lại tên package cho đúng với quy tắc Naming convention

--- src/main/commandline/Dictionary.java
+++ src/main/commandline/Dictionary.java
@@ -1,4 +1,4 @@
-package main.Command_line;
+package main.commandline;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/main/commandline/Test.java
@@ -0,0 +1,12 @@
+package main.commandline;
+
+import static main.commandline.DictionaryCommandLine.dictionaryAdvanced;
+import static main.commandline.DictionaryCommandLine.dictionaryBasic;
+
+public class Test {
+
+    public static void main(String[] args){
+        //dictionaryBasic();
+        dictionaryAdvanced();
+    }
+}
