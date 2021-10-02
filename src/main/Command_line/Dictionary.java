f2a378152b0fad33937500d8eca9ca1aab8fe34f
Đoàn Thanh Sơn <sondt.uet@gmail.com>
2021-10-02 22:29:48 +0700

    change all to static

--- src/main/Command_line/Dictionary.java
+++ src/main/Command_line/Dictionary.java
@@ -4,17 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dictionary {
-    private List<Word> words;
+    public static List<Word> words = new ArrayList<>();
 
-    Dictionary(){
-        words = new ArrayList<>();
-    }
-
-    public List<Word> getWords() {
+    public static List<Word> getWords() {
         return words;
     }
 
-    public void setWords(List<Word> words) {
-        this.words = words;
+    public static void addNewWord(Word new_word) {
+        words.add(new_word);
     }
+
 }
